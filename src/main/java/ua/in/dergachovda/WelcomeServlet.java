5c440c8e2b97ee5119a70207eac7a394075a3631
Dima D. at work <dergachovda@gmail.com>
2016-07-26 16:26:11 +0300

    add ContentType("text/html")

--- src/main/java/ua/in/dergachovda/WelcomeServlet.java
+++ src/main/java/ua/in/dergachovda/WelcomeServlet.java
@@ -11,6 +11,7 @@ public class WelcomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println("<html>");
         writer.println("<head>");
