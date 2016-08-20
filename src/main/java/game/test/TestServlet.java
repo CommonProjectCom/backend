36767cd78a4c3dc0c47126d53610b365e789a0db
Dima D. at work <dergachovda@gmail.com>
2016-08-20 17:54:14 +0300

    Deploy

--- src/main/java/game/test/TestServlet.java
+++ src/main/java/game/test/TestServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/game/trash/test")
+@WebServlet("/test")
 public class TestServlet extends HttpServlet {
 
     public TestServlet() {
